427b4bffad6f793dc8933dae79f5ad594721df7e
Anubhav Mishra <mishraanubhav8126@gmail.com>
2024-08-21 02:10:39 +0530

    updates

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Chai aur Daaru")
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Its my server')
+})
+
+app.get('/youtube', (req, res) => {
+  res.send('Its my City')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
